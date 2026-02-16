Add the masking methods and fix dataloading. Finalize the production ready JEPA without scientfic ablations

train.py
@@ -19,11 +19,23 @@ from wavjepa.types import TransformerEncoderCFG, TransformerLayerCFG
 
 # Component registries
 NETWORKS = {"JEPA": JEPA}
-MASKERS = {"random-masker": RandomMaskMaker, 'random-cluster-masker': RandomClusterMaskMaker, 'time-inverse-masker' : TimeInverseBlockMasker, 'multi-block-masker': MultiBlockMaskMaker}
-EXTRACTORS = {"spatial-conv-extractor": ConvFeatureExtractor, 
-              "conv-extractor": ConvFeatureExtractor}
-
-ENCODERS = {"Transformer" : {"LayerCFG" : TransformerLayerCFG, "EncoderCFG": TransformerEncoderCFG}}
+MASKERS = {
+    "random-masker": RandomMaskMaker,
+    "random-cluster-masker": RandomClusterMaskMaker,
+    "time-inverse-masker": TimeInverseBlockMasker,
+    "multi-block-masker": MultiBlockMaskMaker,
+    "speech-masker": SpeechMaskMaker
+}
+EXTRACTORS = {
+    "wav2vec2": ConvFeatureExtractor,
+    "wavjepa": ConvFeatureExtractor,
+}
+ENCODERS = {
+    "Transformer": {
+        "LayerCFG": TransformerLayerCFG,
+        "EncoderCFG": TransformerEncoderCFG,
+    }
+}
 
 torch.set_float32_matmul_precision("medium")
 torch.backends.cuda.matmul.allow_tf32 = True
@@ -108,6 +120,15 @@ class ComponentFactory:
                 ratio_cutoff = cfg.masker.ratio_cutoff,
                 channel_based_masking = cfg.masker.channel_based_masking,
             )
+        elif cfg.masker.name == "speech-masker":
+            return masker_class(
+                target_masks_per_context=cfg.masker.target_masks_per_context,
+                target_prob=cfg.masker.target_prob,
+                target_length=cfg.masker.target_length,
+                ratio_cutoff=cfg.masker.ratio_cutoff,
+                channel_based_masking=cfg.masker.channel_based_masking,
+                min_context_len = cfg.masker.min_context_len
+            )
         else:
             return masker_class(
                 mask_prob = cfg.masker.mask_prob,
@@ -151,7 +172,7 @@ def setup_logger(cfg) -> TensorBoardLogger:
     """Set up TensorBoard logger with proper configuration."""
     identity = get_identity_from_cfg(cfg)
     return TensorBoardLogger(
-        f"{cfg.save_dir}/tb_logs_jepa_reverted/",
+        f"{cfg.save_dir}/tb_logs_jepa_libri",
         name=identity.replace("_", "/"),
     )
 
@@ -161,7 +182,7 @@ def setup_callbacks(cfg):
     identity = get_identity_from_cfg(cfg)
     
     checkpoint_callback = ModelCheckpoint(
-        dirpath=f"{cfg.save_dir}/saved_models_jepa_reverted/{identity.replace('_', '/')}",
+        dirpath=f"{cfg.save_dir}/saved_models_jepa_libri/{identity.replace('_', '/')}",
         filename="{step}",
         verbose=True,
         every_n_train_steps=25000,
@@ -258,8 +279,8 @@ def main(cfg):
         print_training_info(cfg)
         
         # Start training
-        trainer.fit(model, data_module, ckpt_path=None)
-        
+        trainer.fit(model, data_module)
+
     except Exception as e:
         print(f"Training failed with error: {str(e)}")
         raise

wavjepa/audio_masking.py
@@ -213,7 +213,7 @@ def compute_mask_indices(
             )
 
         mask_idc = np.unique(mask_idc[mask_idc < sz])
-        if len(mask_idc) >= sz:
+        if len(mask_idc) > sz:
             raise ValueError(
                 (
                     f"the entire sequence is masked. "